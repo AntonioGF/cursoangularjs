78bd376e9a873cc78b585406cab9fd82a6c6ada0
AntonioGF <l.antoniogf30@gmail.com>
2017-07-16 21:16:24 -0400

    Add files via upload

---
+++ hola mundo.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+    var nombre="\n Luis",Profesion="\n Ingeniero en Sistemas", Edad=20, Nivel=0.5*10 ,Expectativas="\n Adquirir conocimiento de los tipos de lenguajes de programacion que existen...";
+
+    console.log("Nombre: ", nombre);
+    console.log("Profesion: ", Profesion);
+    console.log("Edad: \n", Edad);
+    console.log("Nivel de conocimientos de programación del 1 al 10: \n ", Nivel);
+    console.log("Expectativas que tienes del cuerso:", Expectativas);
+
+}) ();
